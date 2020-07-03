20b6a1683fb83d980e3864a908bd7d8ba413895c
holzerV <59916228+holzerV@users.noreply.github.com>
2020-07-03 14:03:07 +0300

    forms finished

--- src/Components/Home/Login/Login.tsx
+++ src/Components/Home/Login/Login.tsx
@@ -1,11 +1,57 @@
 import * as React from "react";
+import "./Login.css";
+import { Field, Formik } from "formik";
 
 export interface LoginProps {}
 
 const Login: React.SFC<LoginProps> = () => {
+  const [error, setError] = React.useState("");
+
   return (
-    <div>
-      <div></div>
+    <div className="login">
+      <div className="login-container">
+        <Formik
+          initialValues={{ login: "", password: "" }}
+          onSubmit={(data) => {
+            console.log(data);
+          }}
+        >
+          {({ values, handleChange, handleSubmit }) => (
+            <form className="login-form" onSubmit={handleSubmit}>
+              <h2 style={{ margin: 0, textAlign: "center" }}>Sign up</h2>
+              <div className="input-container">
+                <label htmlFor="login">
+                  <b>Enter your login:</b>
+                </label>
+                <Field
+                  className="input"
+                  id="login"
+                  placeholder="login"
+                  value={values.login}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="input-container">
+                <label htmlFor="password">
+                  <b>Enter your password:</b>
+                </label>
+                <Field
+                  type="password"
+                  className="input"
+                  id="password"
+                  placeholder="password"
+                  value={values.password}
+                  onChange={handleChange}
+                />
+              </div>
+              <span className="btn" onClick={() => handleSubmit()}>
+                Log in
+              </span>
+              <strong>{error}</strong>
+            </form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 };
--- src/Components/Home/Signup/Signup.tsx
+++ src/Components/Home/Signup/Signup.tsx
@@ -1,29 +1,35 @@
 import * as React from "react";
 import "./Signup.css";
-import { Formik } from "formik";
+import { Formik, Field } from "formik";
 
 export interface SignupProps {}
 
 const Signup: React.SFC<SignupProps> = () => {
-  const handleSubmit = (data: any) => {
-    console.log(data);
-  };
+  const [error, setError] = React.useState("");
 
   return (
     <div className="signup">
       <div className="signup-container">
         <Formik
-          initialValues={{ login: "", password: "", repeatPassowrd: "" }}
-          onSubmit={handleSubmit}
+          initialValues={{ login: "", password: "", repeatPassword: "" }}
+          onSubmit={(data) => {
+            if (data.login.length === 0) {
+              setError("login is required");
+            } else if (data.password.length < 6) {
+              setError("your password is too short");
+            } else if (data.password !== data.repeatPassword) {
+              setError("your password do not match");
+            } else console.log(data);
+          }}
         >
-          {({ values, handleChange }) => (
-            <form className="signup-form">
+          {({ values, handleChange, handleSubmit }) => (
+            <form className="signup-form" onSubmit={handleSubmit}>
               <h2 style={{ margin: 0, textAlign: "center" }}>Sign up</h2>
               <div className="input-container">
                 <label htmlFor="login">
                   <b>Enter your login:</b>
                 </label>
-                <input
+                <Field
                   className="input"
                   id="login"
                   placeholder="login"
@@ -31,9 +37,36 @@ const Signup: React.SFC<SignupProps> = () => {
                   onChange={handleChange}
                 />
               </div>
-              <span className="btn" onClick={handleSubmit}>
-                Submit
+              <div className="input-container">
+                <label htmlFor="password">
+                  <b>Enter your password:</b>
+                </label>
+                <Field
+                  type="password"
+                  className="password"
+                  id="password"
+                  placeholder="password"
+                  value={values.password}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="input-container">
+                <label htmlFor="repeatPassword">
+                  <b>Re-enter your password:</b>
+                </label>
+                <Field
+                  type="password"
+                  className="input"
+                  id="repeatPassword"
+                  placeholder="repeat password"
+                  value={values.repeatPassword}
+                  onChange={handleChange}
+                />
+              </div>
+              <span className="btn" onClick={() => handleSubmit()}>
+                Sign up
               </span>
+              <strong>{error}</strong>
             </form>
           )}
         </Formik>
